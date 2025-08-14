274d10ffb70cd7cd748dc8b26d8a497f8f30a0b1
Hri <hri.gh@outlook.com>
2025-08-14 11:25:03 +0530

    Add Text-to-Speech service with Docker and integrate OpenAI
    - Created Dockerfile for TTS models and runtime environment.
    - Added README.md for deployment instructions and usage.
    - Configured environment settings in env_config.json.
    - Implemented resource requirements for TTS models in resourceRequirements.py.
    - Defined session pool policies in sessionPools.py and sessionPools.yaml.
    - Developed prepareModels.sh for model initialization.
    - Added Flask server (server.py) to handle speech-to-text and text-to-speech requests.
    - Implemented frontend with HTML, CSS, and JavaScript for user interaction.
    - Integrated OpenAI API for processing user messages and generating responses.
    - Included requirements.txt for necessary Python packages.

---
+++ 1-Image-Captioning-App/image_cap.py
@@ -0,0 +1,34 @@
+# v1: Introduction to Generate image captions with the BLIP model
+
+import requests
+from PIL import Image # Python Imaging Library (PIL, is used to open the image file and convert it into an RGB format)
+from transformers import AutoProcessor, BlipForConditionalGeneration
+
+# Load the pretrained processor and model
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+
+# Load your image, DON'T FORGET TO WRITE YOUR IMAGE NAME
+img_path = "./img3.jpeg"
+# convert it into an RGB format 
+image = Image.open(img_path).convert('RGB')
+
+# You do not need a question for image captioning
+text = "the image of"
+
+# The pre-processed image is passed through the processor to generate inputs in the required format.
+inputs = processor(images=image, text=text, return_tensors="pt") #The return_tensors argument is set to "pt" to return PyTorch tensors
+
+# print (inputs) # To see the format (inputs dictionary)
+
+# Generate a caption for the image
+outputs = model.generate(**inputs, max_length=50)
+
+# print(output) # To check the complete output
+
+# Decode the generated tokens to human-readable text
+caption = processor.decode(outputs[0], skip_special_tokens=True) #skip_special_tokens argument is set to True to ignore special tokens in the output text.
+
+# Print the caption
+print(caption)
